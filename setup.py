f59ee8877fa4141ea499e508a3602265b2380f90
Rubel <rubel.dev1121@gmail.com>
2025-12-19 17:24:34 +0600

    setup done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa_approval_pred",
+    version="0.0.0",
+    author="Rubel",
+    author_email="rubel190024@gmail.com",
+    packages=find_packages()
+)
